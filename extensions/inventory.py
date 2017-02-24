7ece2b9b3a0e8441fbabf0d7d0912f7daa5e698e
Andre Segura <andrensegura@gmail.com>
2017-02-24 17:52:32 -0500

    inventory -> inv

--- extensions/inventory.py
+++ extensions/inventory.py
@@ -9,7 +9,7 @@ class Inventory():
         self.bot = bot
 
     @commands.group(pass_context=True)
-    async def inventory(self, ctx):
+    async def inv(self, ctx):
         """View/manipulate your inventory."""
         if ctx.invoked_subcommand is None:
             await self.bot.say("See '.help inventory'")
